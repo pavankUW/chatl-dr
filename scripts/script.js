4cb6fa51687aea322a639f1956a119d902d2cca8
Aashray Anand <Aashray_anand@hotmail.com>
2015-10-18 07:13:28 -0700

    Created query string to redirect user to chat from main screen, jquery enabled to add name room and pass variables as well as action type

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
 $('#Create').click(function(){
 	alert("hello");
+	var Name = ($('#Name').val());
+	var Room = ($('#Room').val())
+	var Pass =($('#Pass').val())
+	var x = "Chatldr.me?Action=Create?Name=" + Name + "?Room="+ Room + "?Pass=" + Pass ;
+	console.log(x);
 });
 $('#Join').click(function(){
-	alert("hello");
+	var Name = ($('#Name').val());
+	var Room = ($('#Room').val())
+	var Pass =($('#Pass').val())
 });
