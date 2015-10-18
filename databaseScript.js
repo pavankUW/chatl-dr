ba83db0048c84b16b8ef8718193c4aaf43706030
AaronJohnston <aaron.johnston001@gmail.com>
2015-10-18 03:15:55 -0700

    sending and receiving chats from firebase

--- databaseScript.js
+++ databaseScript.js
@@ -1,10 +1,29 @@
 $(document).ready(function () {
 
-    $("#submitInput").click(function () {
-        
-        fb.push({
-            
+    function submitMessage() {
+        fb.child("UID").child("GROUPNAME").push({
+            name: "USERNAME",
+            msg: $("#messageInput").val(),
+            time: Date.now()
         });
+        $("#messageInput").val("");
+    }
 
+    function loadMessages(snapshot) {
+        $("#messages").append('<p class="inMessage"><b>'+snapshot.child("name").val()+'</b> '+snapshot.child("msg").val()+'</p>');
+    }
+
+    $("#messageInput").keydown(function (e) {
+        if(e.keyCode == 13) {
+            submitMessage();
+        }
+        
     });
+
+    $("#submitInput").click(submitMessage);
+    
+    fb.child("UID").child("GROUPNAME").on("child_added", function(snapshot, prevChildKey) {
+        loadMessages(snapshot);
+    });
+    
 });
